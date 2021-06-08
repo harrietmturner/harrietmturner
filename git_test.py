c895f6bb23546f6564a98de3d363909bc128a7db
Harriet Turner <h.turner3@pgr.reading.ac.uk>
2021-06-08 09:12:29 +0100

    Second commit

---
+++ git_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 09:06:19 2021
+
+@author: harri
+"""
+
+def bigger_than_five(n):
+    if n > 5:
+        return "Bigger than five"
+    if n == 5:
+        return "Five"
+    else:
+        return "Smaller than five"
